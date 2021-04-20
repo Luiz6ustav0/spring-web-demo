9140147fecbf11d5ce8a61667fb8fcb0d7af0125
Luiz Almeida <luizorst@hotmail.com>
2021-04-20 11:37:11 -0300

    [ADD] add students to database on initialization

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.AUGUST;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student luiz = new Student(
+                    "Luiz",
+                    "luizorst@hotmail.com",
+                    LocalDate.of(2000, AUGUST, 30),
+                    20
+            );
+            Student maria = new Student(
+                    "Maria",
+                    "maria@hotmail.com",
+                    LocalDate.of(2001, AUGUST, 30),
+                    19
+            );
+
+            repository.saveAll(List.of(luiz, maria));
+        };
+    }
+}
